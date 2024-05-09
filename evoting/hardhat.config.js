1657430b524a6be49508d97d4a566a4a4b5a482a
Enes Önal <enes.onal777@gmail.com>
2024-05-09 18:26:18 +0300

    refact: change account balance

--- evoting/hardhat.config.js
+++ evoting/hardhat.config.js
@@ -2,16 +2,14 @@ require("@matterlabs/hardhat-zksync-solc");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
   networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk',
+        count: 10,
+        accountsBalance: '200000000000000000000000'
+      }
+    },
     zksync_testnet: {
       url: "https://zksync2-testnet.zksync.dev",
       ethNetwork: "goerli",
@@ -25,19 +23,5 @@ module.exports = {
       zksync: true,
     },
   },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
-  solidity: {
-    version: "0.8.17",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+  solidity: "0.8.17",
 };
