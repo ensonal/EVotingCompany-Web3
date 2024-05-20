0ea1edd17ca5abae36cce19bf94c731d21be2ece
yaren <yarengokhn@gmail.com>
2024-05-20 14:51:28 +0300

    delegate function added to contract.

--- evoting/contracts/Contract.sol
+++ evoting/contracts/Contract.sol
@@ -2,38 +2,35 @@
 pragma solidity ^0.8.9;
 
 contract Evoting {
-    // a struct to represent a voter
-    // vote and weight are declared one n' after so that we can couple them. This will affect the ethereum optimization[source: Cryptozombies]
+    // Struct to represent a voter
     struct Voter {
-        uint vote;
-        uint weight;
-        bool voted;
+        uint vote; // Index of the voted proposal
+        uint weight; // Weight of the vote
+        bool voted; // True if the voter has already voted
     }
 
-    // a struct to represent a proposals.
-    // Proposals are the things we are voting for
-    // name is a byte, because of this you will need to convert between string and bytes before using
+    // Struct to represent a proposal
     struct Proposal {
-        bytes32 name;
-        uint voteCount;
+        bytes32 name; // Short name of the proposal
+        uint voteCount; // Number of accumulated votes
     }
     
-    // a public array to keep record of proposals
+    // Array of proposals
     Proposal[] public proposals;
     
-    // a public hashmap to keep record of voters
+    // Mapping to store voters' information
     mapping(address => Voter) public voters;
 
-    // address of the person who started the voting, chairwoman-chairman
+    // Address of the person who started the voting process (chairperson)
     address public chairperson;
 
-    // This constructor initializes chairperson variable and voters, proposals arrays-hashmaps
+    // Constructor to initialize the contract
     constructor(bytes32[] memory proposalNames) {
         chairperson = msg.sender;
 
-        voters[chairperson].weight = 1;
+        voters[chairperson].weight = 1; // Chairperson starts with a weight of 1
 
-        for(uint i = 0; i < proposalNames.length; i++) {
+        for (uint i = 0; i < proposalNames.length; i++) {
             proposals.push(Proposal({
                 name: proposalNames[i],
                 voteCount: 0
@@ -41,37 +38,55 @@ contract Evoting {
         } 
     }
 
-    // This function despite being a public function, is going to be used by the owner to give rights to people to vote
+    // Function to give the right to vote to a shareholder
     function giveRightToVote(address voter) public {
         require(msg.sender == chairperson, "Only the Chairperson can give access to vote");
-        // require that the voter hasn't voted yet
         require(!voters[voter].voted, "The voter has already voted");
-        require(voters[voter].weight == 0);
-        voters[voter].weight = 1;
+        require(voters[voter].weight == 0, "Voter's weight must be zero");
+        
+        // Set the voting weight based on the voter's shares (assumed to be 1 for simplicity)
+        voters[voter].weight = 1; // In practice, set this to the number of shares the voter holds
     }
 
-    // This function is public and can be used by anyone to vote, but of course if you are not eligible to vote it will raise an exception
+    // Function for shareholders to vote on a proposal
     function vote(uint proposal) public {
         Voter storage sender = voters[msg.sender];
+        
         require(sender.weight != 0, "Has no right to vote");
         require(!sender.voted, "Already voted");
+
         sender.voted = true;
         sender.vote = proposal;
 
         proposals[proposal].voteCount += sender.weight;
     }
 
-    // this function can be queried after the vote ends
+    // Function to delegate vote to another voter
+    function delegate(address to) public {
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted, "You have already voted");
+        require(sender.weight != 0, "You have no right to vote");
+
+        Voter storage delegateTo = voters[to];
+        require(!delegateTo.voted, "Delegatee has already voted");
+
+        delegateTo.weight += sender.weight;
+        sender.weight = 0;
+    }
+
+    // Function to get the index of the winning proposal
     function winningProposal() public view returns (uint winningProposal_) {
         uint winningVoteCount = 0;
-        for(uint i = 0; i < proposals.length; i++) {
+        
+        for (uint i = 0; i < proposals.length; i++) {
             if (proposals[i].voteCount > winningVoteCount) {
                 winningVoteCount = proposals[i].voteCount;
                 winningProposal_ = i;
             }
         }
     }
-    // this function can be queried after the vote ends and it will return the name of the winning proposal
+
+    // Function to get the name of the winning proposal
     function winningName() public view returns (bytes32 winningName_) {
         winningName_ = proposals[winningProposal()].name;
     }
